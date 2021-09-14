a1ece51030bde87caadeda7fcdb772301596110d
Mr. Hullen <48824760+MrHullen@users.noreply.github.com>
2021-09-14 13:06:49 +1200

    adds projects

---
+++ code/3-conversation.js
@@ -0,0 +1,15 @@
+// ####################################
+// ##### ----- Conversation ----- #####
+// ####################################
+
+function conversation() {
+  alert('Welcome to my conversation program.')
+  alert('Do you like cycling? Answer yes or no.')
+  let answer = prompt()
+  if (answer == 'yes') {
+    alert("That's good - you will get very fit.")
+  } else {
+    alert('Perhaps you like some other sport.')
+  }
+  alert('Goodbye')
+}
